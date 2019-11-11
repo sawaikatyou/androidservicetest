7fd6f91134386846c4bad15d4110b2b99a4cf26d
sasakik <mega_red@mac.com>
2019-11-12 00:00:57 +0900

    service binding check

--- app/src/main/java/com/sawaikatyou/androidservicetest/MainActivity.java
+++ app/src/main/java/com/sawaikatyou/androidservicetest/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,16 +21,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mMainServiceConnection = new MainServiceConnection(this);
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mMainServiceConnection != null) {
+            unbindService(mMainServiceConnection);
+            mMainServiceConnection = null;
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        reloadButtonState();
+    }
+
+    void reloadButtonState() {
+        boolean isbinded = (mMainServiceConnection != null);
+        if (isbinded) {
+            findViewById(R.id.button_bindservice).setEnabled(false);
+            findViewById(R.id.button_executeservice).setEnabled(true);
+            findViewById(R.id.button_unbindservice).setEnabled(true);
+        } else {
+            findViewById(R.id.button_bindservice).setEnabled(true);
+            findViewById(R.id.button_executeservice).setEnabled(false);
+            findViewById(R.id.button_unbindservice).setEnabled(false);
+        }
+    }
 
     static class MainServiceConnection implements ServiceConnection {
-        Activity mParentActivity;
+        MainActivity mMainActivity;
         MainService mConnectedService;
 
-        MainServiceConnection(Activity act) {
-            mParentActivity = act;
+        MainServiceConnection(MainActivity act) {
+            mMainActivity = act;
             mConnectedService = null;
         }
 
@@ -37,10 +64,12 @@ public class MainActivity extends AppCompatActivity {
         public void onServiceConnected(ComponentName name, IBinder service) {
             MainService.LocalBinder binder = (MainService.LocalBinder) service;
             mConnectedService = binder.getService();
+            mMainActivity.reloadButtonState();
         }
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
+            mMainActivity.reloadButtonState();
             mConnectedService = null;
         }
 
@@ -63,6 +92,11 @@ public class MainActivity extends AppCompatActivity {
                 stopService(intent);
                 break;
             case R.id.button_bindservice:
+                if (mMainServiceConnection != null) {
+                    unbindService(mMainServiceConnection);
+                    mMainServiceConnection = null;
+                }
+                mMainServiceConnection = new MainServiceConnection(this);
                 bindService(intent, mMainServiceConnection, Context.BIND_AUTO_CREATE);
                 break;
             case R.id.button_executeservice:
@@ -73,6 +107,8 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.button_unbindservice:
                 unbindService(mMainServiceConnection);
+                mMainServiceConnection = null;
+                reloadButtonState();
                 break;
             default:
                 break;
---
+++ app/src/test/java/com/sawaikatyou/androidservicetest/MainActivityTest.java
@@ -0,0 +1,14 @@
+package com.sawaikatyou.androidservicetest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainActivityTest {
+
+    @Test
+    public void addition_isCorrect() {
+        MainActivity act = new MainActivity();
+        assertEquals(4, 2 + 2);
+    }
+}
